327f645a010a8f02b01d83f888557c8f2db3ad30
Daryl Stensland <dstensland5@gmail.com>
2023-05-16 11:16:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello from server', status: 200 });
+});
+
+app.post('/', (req, res) => {
+  res.send('Posted');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('App running on port: 3000');
+});
