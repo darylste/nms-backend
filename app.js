f33e0e4f6c17c691bf1bfb77565c45550a32bb3e
Daryl Stensland <dstensland5@gmail.com>
2023-05-16 12:14:04 +0100

    dummy create event route made

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const express = require('express');
 
 const app = express();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/events.js`));
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/events.json`));
 
 app.get('/api/v1/events', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours },
+  });
+});
+
+app.post('/api/v1/events', (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      event: req.body,
+    },
   });
 });
 
